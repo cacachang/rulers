9b17bbcc6a1b6e1f8dc94c21ce26426cf3d93012
Ning <a24701770@gmail.com>
2024-04-04 23:11:13 +0800

    add: application class and call proc to handle http request

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,6 +3,12 @@
 require_relative "rulers/version"
 
 module Rulers
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+    end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
